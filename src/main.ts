117a944f3d8efbabab51e61a09190ca6a9227131
marcogael1 <132959641+marcogael1@users.noreply.github.com>
2025-03-25 14:36:56 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 import * as fs from 'fs';
 import { LogService } from './services/logs.service';
 import { GlobalExceptionFilter } from './filters/global-exception.filter';
